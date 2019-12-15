1ddd7e8e171cee59313a27085c3b17f422dcbe61
Fernando Teles <fernandotalves@gmail.com>
2019-12-15 11:29:26 -0300

    create a task model

---
+++ src/models/task.py
@@ -0,0 +1,11 @@
+from config import db
+
+class Task(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    description = db.Column(db.String(80), unique=True, nullable=False)
+    state = db.Column(db.SMALLINT, nullable=False, default=0)
+    user_id = db.Column(db.Integer, nullable=False)
+
+    def __repr__(self):
+        return '<Task (id=%r, description=%r, state=%r, user_id=%r)>' \
+                % (self.id, self.description, self.state, self.user_id)
