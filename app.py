e6d295e9b75174c0cc72845439f1036ccdf10520
Fernando Teles <fernandotalves@gmail.com>
2019-12-15 11:46:00 -0300

    extract task service to a module

--- app.py
+++ app.py
@@ -1,13 +1,10 @@
 from config import app
-from flask_restful import Resource, Api
+from flask_restful import Api
+from src.services.task_service import TaskList
 
 api = Api(app)
 
-class TaskService(Resource):
-    def get(self):
-        return '', 200
-    
-api.add_resource(TaskService, '/v1/tasks/')
+api.add_resource(TaskList, '/v1/tasks/')
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
