9ac89d4e238535d9981d5b90a2d2c171004293c1
Fernando Teles <fernandotalves@gmail.com>
2019-12-15 18:22:44 -0300

    add config to deploy on heroku

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from config import app
 from flask_restful import Api
 from src.services.task_service import TaskList, TaskService
@@ -8,4 +9,5 @@ api.add_resource(TaskList, '/v1/tasks/')
 api.add_resource(TaskService, '/v1/tasks/<task_id>')
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
--- config.py
+++ config.py
@@ -1,6 +1,12 @@
+import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/tasks.db'
+db_path = os.path.join(os.path.dirname(__file__), 'tasks.db')
+db_uri = 'sqlite:///{}'.format(db_path)
+app.config['SQLALCHEMY_DATABASE_URI'] = db_uri
+app.config['SECRET_KEY'] = '6a3056ebd17c729caeca5742'
 db = SQLAlchemy(app)
+CORS(app)
